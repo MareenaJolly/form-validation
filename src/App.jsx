d4e346ea542774aa4ad741f8725c5f9126f76b40
mareena <mereena333@gmail.com>
2024-09-22 22:20:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import FormWithYup from './components/Form-with-yup'
+import FormWithoutYup from './components/Form-without-yup'
+
+function App() {
+  return (
+    <div>
+     { /*<FormWithoutYup />*/}
+     <FormWithYup />
+    </div>
+  )
+}
+
+export default App
